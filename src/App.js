6251123951ba7e1f905ef59d773cea22419d7575
Cameron Lares <cameronlares@gmail.com>
2020-06-10 02:34:36 -0400

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
+
+import PostsPage from "./components/PostsContainer/PostsPage.js";
+import SearchBarContainer from "./components/SearchBar/SearchBarContainer.js";
 import React from "react";
 import "./App.css";
+
+
 // import the PostsPage and SearchBar and add them to the App
 
-
 const App = () => {
-
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBarContainer />
+      <PostsPage />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
-
+// import data
+import dummyData from "../../dummy-data.js";
 const PostsPage = () => {
   // set up state for your data
+  const [postsData, setPostsData] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {postsData.map((postData,index) => {
+
+        return <Post key={index}  post={postData} />;
+      })}
     </div>
   );
 };
